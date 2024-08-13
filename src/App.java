e1537994036af805e9301309c414612cee7c92c1
Ju-Ribeiroo <julia04ribeiro05@gmail.com>
2024-08-13 12:00:40 -0300

    Testes transferencia arquivos reposit local para repositorio remoto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Sejam Bem-vindos!");
+        System.out.println("Sejam bem-vindos!");
     }
 }
